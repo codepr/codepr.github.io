4c64457546243b0a296ad8f4f0e72774db8e6399
Andrea Baldan <a.g.baldan@gmail.com>
2018-12-04 00:57:01 +0100

    Changed font for title

--- _assets/javascripts/webfonts.js
+++ _assets/javascripts/webfonts.js
@@ -2,6 +2,6 @@
 
 WebFont.load({
   google: {
-      families: ['Cormorant Garamond:700', 'Lato:300,400,700', 'Inconsolata:300,400,700', 'Roboto:300,400,700', 'Montserrat:300,400,700']
+      families: ['Cormorant Garamond:700', 'Lato:300,400,700', 'Inconsolata:300,400,700', 'Roboto:300,400,700', 'Montserrat:300,400,700', 'Cinzel:300,400,700', 'Mr Dafoe:300,400,700']
   }
 });
