ef6f54742960b81ee101b319a1d7cb51377b34a1
Andrea Baldan <a.g.baldan@gmail.com>
2019-05-01 23:15:47 +0200

    Update css spacing between articles [ci skip]

--- _assets/javascripts/webfonts.js
+++
@@ -1,7 +0,0 @@
-//= require webfontloader/webfontloader.js
-
-WebFont.load({
-  google: {
-      families: ['Cormorant Garamond:700', 'Lato:300,400,700', 'Inconsolata:300,400,700', 'Roboto:300,400,700', 'Montserrat:300,400,700', 'Cinzel:300,400,700', 'Mr Dafoe:300,400,700']
-  }
-});
--- _my_tags/tutorial.md
+++
@@ -1,4 +0,0 @@
----
-slug: tutorial
-name: Tutorial
----
